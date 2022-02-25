8939bfeb85188576962ad376013d688476936a31
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-25 06:05:16 -0800

    string examples and word spell checker

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -12,15 +12,26 @@ struct ContentView: View {
         
     }
     
-    func loadFile() {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            //here means file was found inside installed app bundle on user device and can be loaded
-            if let fileContents = try? String(contentsOf: fileURL) {
-                //here means loaded file into string fileContents
-            }
-        }
+    func test(){
+        let input = """
+a
+b
+c
+"""
+        let letters = input.components(separatedBy: "\n")
+        let letter = letters.randomElement()
+        //removes white space and new line
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        let word = "swift"
+        let checker = UITextChecker()
+        //.utf16 is a character encoding to allow for Objective C to understand Swift strings
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        //checks if mispelledRange comes back empty
+        let allGood = misspelledRange.location == NSNotFound
     }
-}
+}e
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
