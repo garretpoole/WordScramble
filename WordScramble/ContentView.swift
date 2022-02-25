82d5dc205b82dbc342f760e6c1031515edeb0013
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-25 05:41:14 -0800

    dynamic and static list examples

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Fin", "Leia", "Luke", "Rey"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        List(people, id: \.self){
+//            Text("\($0)")
+        List{
+            Text("Static Row 1")
+            ForEach(people, id: \.self){
+                Text("\($0)")
+            }
+            Text("Static Row 2")
+            Text("Static Row 3")
+            
+        }
+        .listStyle(.grouped)
     }
 }
 
