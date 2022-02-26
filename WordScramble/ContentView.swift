02d8cf62d1a6cb659d95b0ff77247967082f6e27
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-26 15:30:10 -0800

    root word list incorporated, pulls random word from list in start.txt

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -30,7 +30,18 @@ struct ContentView: View {
                 }
             }
             .navigationTitle(rootWord)
+            .onAppear(perform: startGame)
         }
+    }
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let startWords = try? String(contentsOf: startWordsURL){
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        fatalError("Could not load start.txt from the bundle")
     }
     
     func addNewWord() {
